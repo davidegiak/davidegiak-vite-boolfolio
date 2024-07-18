758ac09415e7c26c33d9daf56ae0b606180ea70d
Davide Di Giacomo <davidedigiacomo001@gmail.com>
2024-07-18 18:26:23 +0200

    ..

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,27 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
 
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      projects: ''
+    }
+   
+  },
+
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/projects').then(response => {
+      console.log(response);
+      this.projects = response.data.projects
+    })
+  }
+}
+</script>
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" /> GRRR
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  {{ projects }}
 </template>
 
 <style scoped>
@@ -26,5 +36,8 @@ import HelloWorld from './components/HelloWorld.vue'
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+template{
+  color: white;
 }
 </style>
